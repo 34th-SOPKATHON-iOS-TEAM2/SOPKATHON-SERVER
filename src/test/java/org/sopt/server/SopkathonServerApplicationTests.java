4dbeda3a59a25c8855877ec9577696f92c63df35
nykoh2001 <nykoh2001@gmail.com>
2024-05-17 21:56:54 +0900

    initial commit

---
+++ src/test/java/org/sopt/server/SopkathonServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SopkathonServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
