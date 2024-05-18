8e1928fbb2246bebdd218ab6025af588af3f3ac8
nykoh2001 <nykoh2001@gmail.com>
2024-05-19 04:00:18 +0900

    [FEAT] #14 get attempt logs API

--- src/main/java/org/sopt/server/repository/AttemptRepository.java
+++ src/main/java/org/sopt/server/repository/AttemptRepository.java
@@ -2,15 +2,19 @@ package org.sopt.server.repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import org.sopt.server.domain.Attempt;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AttemptRepository extends JpaRepository<Attempt, Long> {
 
     @Query("SELECT a FROM Attempt a WHERE a.member.id = :memberId AND a.attemptedDate = :date")
-    List<Attempt> getAttemptsByMemberIdAndDate(final Long memberId, final LocalDate date);
+    List<Attempt> findAttemptsByMemberIdAndDate(final Long memberId, final LocalDate date);
 
+    @Query("SELECT a FROM Attempt a WHERE a.member.id = :memberId")
+    List<Attempt> findAttemptsByMemberId(final Long memberId);
 }
