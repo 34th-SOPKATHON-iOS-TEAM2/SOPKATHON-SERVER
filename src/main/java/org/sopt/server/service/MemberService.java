427bde744b56f368919391f75b0c3f663a703b12
nykoh2001 <nykoh2001@gmail.com>
2024-05-19 01:20:15 +0900

    [FEAT] #6 check duplicate member

--- src/main/java/org/sopt/server/service/MemberService.java
+++ src/main/java/org/sopt/server/service/MemberService.java
@@ -1,9 +1,12 @@
 package org.sopt.server.service;
 
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.sopt.server.domain.Member;
 import org.sopt.server.dto.request.MemberCreateRequest;
 import org.sopt.server.dto.response.MemberCreateResponse;
+import org.sopt.server.exception.CommonException;
+import org.sopt.server.exception.dto.ErrorCode;
 import org.sopt.server.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +18,10 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     public MemberCreateResponse saveMember(final MemberCreateRequest memberCreateRequest) {
+        Optional<Member> opMember = memberRepository.findByName(memberCreateRequest.name());
+        if (opMember.isPresent()) {
+            throw new CommonException(ErrorCode.DUPLICATE_MEMBER_NAME);
+        }
         return MemberCreateResponse.of(memberRepository.save(Member.from(memberCreateRequest)));
     }
 }
